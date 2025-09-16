af7aa9e9b029b446af90ed53ecda2a4d323db5e4
Abolfazl <38177544+adarvish3@users.noreply.github.com>
2025-09-17 01:55:20 +0330

    Delete school-counselor-pwa/src directory

--- school-counselor-pwa/src/registerSW.js
+++
@@ -1,9 +0,0 @@
-export function registerSW() {
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/sw.js')
-        .then(reg => console.log('Service Worker registered:', reg))
-        .catch(err => console.error('Service Worker failed:', err))
-    })
-  }
-}
