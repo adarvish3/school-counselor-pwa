9d6f8a7011b8ca59d7d429cadb65b463c6eed9da
Abolfazl <38177544+adarvish3@users.noreply.github.com>
2025-09-17 01:45:36 +0330

    Update vite.config.js

--- school-counselor-pwa/vite.config.js
+++ school-counselor-pwa/vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/school-counselor-pwa/',  // نام ریپو
   build: {
-    outDir: 'dist'
-  }
-})
+    outDir: 'dist',
+  },
+}
